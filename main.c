190de320120365048ac6a4a4af61b6193461f121
yeeun105106-arch <yeeun105106@gmail.com>
2026-09-21 17:41:09 +0900

    03

--- main.c
+++ main.c
@@ -2,13 +2,13 @@
 
 int main(void) {
     char c;
-    int i;
+    //int c1;
     
-    printf("input a number :");
+    printf("input a character :");
     scanf("%c", &c);
     
-    i = c - '0';
-    printf("The input number is %i\n", i);
+    //c1 = c +1;
+    printf("The next character of %c (%d) is %c (%d)\n", c, c, c+1, c+1);
 
     return 0;
 }
