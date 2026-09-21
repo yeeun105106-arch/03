dcc298bc28c7b5329cd97f589f56e9597d947e43
yeeun105106-arch <yeeun105106@gmail.com>
2026-09-21 17:51:55 +0900

    04

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
 int main(void) {
-    char c;
+    int a, b;
     //int c1;
     
-    printf("input a character :");
-    scanf("%c", &c);
+    printf("input a number :");
+    scanf("%d", &a);
+
+    printf("input a number :");
+    scanf("%d", &b);
     
     //c1 = c +1;
-    printf("The next character of %c (%d) is %c (%d)\n", c, c, c+1, c+1);
+    printf("Dividing result is %f\n", (float)a/b);
 
     return 0;
 }
